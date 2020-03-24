e71e124e67be01d79fde21877baff93c25c4edec
Scott Harlan <seharlan@gmail.com>
2020-03-24 12:11:59 -0700

    failing tests created

---
+++ __tests__/tweet-routes.test.js
@@ -0,0 +1,85 @@
+require('dotenv').config();
+
+const request = require('supertest');
+const app = require('../lib/app');
+const connect = require('../lib/utils/connect');
+const mongoose = require('mongoose');
+const Tweet = require('../lib/models/Tweet');
+
+
+const testTweet = {
+  handle: 'scott',
+  text: 'this is a test tweet'
+};
+const emptyTweet = {
+  handle: 'scott',
+  text: ''
+};
+
+describe('app routes', () => {
+  beforeAll(() => {
+    connect();
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+
+  it('creates a new tweet', () => {
+    return request(app)
+      .post('/api/v1/tweets')
+      .send(testTweet)
+      .then(result => {
+        expect(result.body).toEqual({ ...testTweet, _id: expect.any(String), __v: 0 });
+      });
+  });
+
+  it('gets all tweets', () => {
+    return Tweet.create(testTweet)
+      .then(res => {
+        return request(app)
+          .get('/api/v1/tweets')
+          .then(result => {
+            expect(result).toEqual([{ ...testTweet, _id: expect.any(String), __v: 0 }]);
+          });
+      });
+  });
+
+  it.skip('gets a tweet by id', () => {
+    return Tweet.create(testTweet)
+      .then(tweet => {
+        return request(app)
+          .get(`/api/v1/tweets/${tweet._id}`)
+          .then(result => {
+            expect(result).toEqual({ ...testTweet, _id: expect.any(String), __v: 0 });
+          });
+      });
+  });
+
+  it.skip('updates a tweeets text only', () => {
+    return Tweet.create(emptyTweet)
+      .then(tweet => {
+        return request(app)
+          .patch(`/api/v1/tweets/${tweet._id}`)
+          .send('new text for tweet')
+          .then(result => {
+            expect(result).toEqual({ handle: 'scott', text: 'new text for tweet', _id: expect.any(String), __v: 0 });
+          });
+      });
+  });
+
+  it.skip('deletes a tweet by id', () => {
+    return Tweet.create(testTweet)
+      .then(tweet => {
+        return request(app)
+          .delete(`/api/v1/tweets/${tweet._id}`)
+          .then(result => {
+            expect(result).toEqual({ ...testTweet, _id: expect.any(String), __v: 0 });
+          });
+      });
+  });
+});
